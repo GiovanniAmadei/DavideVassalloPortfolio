f1eac19dade07f70a75a19aa1bf2cc0b0fda4eab
Giovanni Amadei <giovannisebastianoamadei@gmail.com>
2026-03-12 23:04:20 +0100

    Revert: torno a configurazione Next.js standard senza export

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  if (typeof window !== 'undefined') {
-    window.location.href = '/index.html';
-  }
-  
-  return (
-    <>
-      <meta httpEquiv="refresh" content="0;url=/index.html" />
-      <script dangerouslySetInnerHTML={{ __html: `window.location.href='/index.html'` }} />
-    </>
-  );
+  redirect('/index.html');
 }
