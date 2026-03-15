54e1f94b74e4a64d008952c99ab2dadb89965756
Giovanni Amadei <giovannisebastianoamadei@gmail.com>
2026-03-15 11:19:18 +0100

    fix: next config transpilePackages for cloudinary esm crash

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ['next-tinacms-cloudinary', '@tinacms/auth'],
   typescript: {
     ignoreBuildErrors: true,
   },
